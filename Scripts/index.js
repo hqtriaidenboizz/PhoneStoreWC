8243f6a10dbbdf268f8816b73b9b0175cd3712d3
hqtriaidenboizz <hoquoctri10b7@gmail.com>
2022-08-31 12:05:02 +0700

    update icon

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,4 @@
-window.onscroll = function () { ScrollNav() };
+
 const ScrollNav = () => {
     var x = document.getElementById('header');
     if (document.documentElement.scrollTop > 80) {
@@ -144,5 +144,21 @@ const OpentNavMobile = () =>{
 
 
 // function Icon Navigtion
+var mybutton = document.getElementById("BtnScroll");
 
 
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+window.addEventListener('scroll', ScrollNav);
+window.addEventListener('scroll', scrollFunction);  
